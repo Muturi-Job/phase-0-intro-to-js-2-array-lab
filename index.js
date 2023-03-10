6a9671d4c0b5175c5ec3e0b00ff802b8475d51e7
Job Muturi <muirimuturi17@gmail.com>
2023-03-10 11:19:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    return [ ...cats,"Broom"]
+}
+function removeLastCat(){
+    return cats.slice(0,2)
+}
+function prependCat(){
+    return ["Arnold",...cats]
+}
+function removeFirstCat(){
+    return cats.slice(1,3)
+}
